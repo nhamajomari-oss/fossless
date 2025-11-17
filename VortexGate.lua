9c161c8ba92ee27e4a725811ea1f126d3a7bef82
dark <nhamajomari@gmail.com>
2025-11-17 08:47:09 +0000

    Update VortexGate.lua

--- VortexGate.lua
+++ VortexGate.lua
@@ -1,4 +1,3 @@
-return
 local WindUI = loadstring(game:HttpGet("https://github.com/Footagesus/WindUI/releases/latest/download/main.lua"))()
 local Window = WindUI:CreateWindow({
     Title = "VortexGate",
